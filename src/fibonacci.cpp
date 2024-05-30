92a66024ab4c256bfb73e2efd5bae1e4ffdf8844
Lucas Llort <lucasllort@gmail.com>
2024-05-29 22:29:45 -0400

    node :P

--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -1,6 +1,13 @@
 #include "tarea.hpp"
 #include "arbol_binomial.cpp"
 #include <limits>
+#include <cmath>
+
+
+
+int log2_ceil(int x) {
+    return static_cast<int>(ceil(log2(x)));
+}
 
 class ColaFibonacci{
     vector <ArbolBinomial>arboles;
@@ -18,11 +25,43 @@ class ColaFibonacci{
             hayArboles++;
         }
         else if(parMinimo.distancia>distancia){
-            minimo=
+            minimo=arbol;
         }
     }
     Par *ExtraerMinimo(){
+        ArbolBinomial arbolMinimo=minimo;
+        Par *resultado=arbolMinimo.raiz;
+        auto it = find(arboles.begin(), arboles.end(), arbolMinimo);
+        arboles.erase(it);
+        int tamaño=arbolMinimo.arboles.size();
 
+        for(int i=0;i<tamaño;i++){
+            arboles.push_back(arbolMinimo[i]);
+            tamaño++;
+        }
+        ArbolBinomial *punteros[log2_ceil(tamaño)]={nullptr};
+        for(int k=0;k<tamaño;k++){
+            if(punteros[arboles[k].orden]=nullptr){
+                punteros[arboles[k].orden]=arboles[k];
+            }
+            else{
+                if(arboles[k].raiz.distancia>punteros[arboles[k].orden]){
+                    arboles[k].arboles.push_back(punteros[arboles[k].orden]);
+                }
+                else{
+                    punteros[arboles[k].orden].arboles.push_back(arboles[k])
+
+                }
+            }
+        }
+
+
+
+        return resultado;
     }
 
 };
+
+void funcion(ArbolBinomial *punteros, ArbolBinomial arboles){
+    
+}
