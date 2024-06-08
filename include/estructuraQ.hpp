b53f02bbeaf9d199bfd78adfd8ade193eb0ff506
Lako-git <joaquin.gonzalez.2@ug.uchile.cl>
2024-06-07 21:45:36 -0400

    Trait estructura Q

---
+++ include/estructuraQ.hpp
@@ -0,0 +1,19 @@
+#ifndef ESTRUCTURAQ_H
+#define ESTRUCTURAQ_H
+
+#include <utility>
+#include "tarea.hpp"  
+
+class estructuraQ {
+public:
+    using Par = logs::Par;
+    using Nodo = logs::Nodo;
+
+    virtual Par ExtraerMinimo() = 0;
+    virtual void AgregarPar(int distancia, Nodo *nodo) = 0;
+    virtual void DecreaseKey(Nodo nodo, int nuevaDistancia) = 0;
+
+    virtual ~estructuraQ() = default;
+};
+
+#endif // ESTRUCTURAQ_H
